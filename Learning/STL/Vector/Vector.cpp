b1905146391336bb767046e098d13a7822e08ff2
Ilie Ioan <ionutilie2002@gmail.com>
2024-10-03 14:00:20 +0300

    Add files via upload

--- Learning/STL/Vector/Vector.cpp
+++ Learning/STL/Vector/Vector.cpp
@@ -2,10 +2,60 @@
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //declaration:
+    vector<int>TestVector1;
+
+    cout << "Size:" << TestVector1.size() << endl; // size function
+
+    //initialization:
+
+    vector<int>TestVector2 = { 10,20,30 };
+
+    //methods
+
+    cout << " At " << TestVector2.at(1) << endl;//out of bound safe
+
+    cout << " [] " << TestVector2[1] << endl;//unsafe
+
+    TestVector2.push_back(40); // inserting an element at the end of the vector
+    TestVector2.push_back(50);
+    TestVector2.push_back(60);
+
+    //iterator is pointer to container
+    for (vector<int>::iterator i = TestVector2.begin(); i != TestVector2.end(); i++)
+    {
+        cout << *i << " ";
+    }
+    cout << endl;
+    
+
+    //insert the element in vector before the position pointed
+    vector<int>::iterator i = TestVector2.begin();
+    TestVector2.insert(i, 0);
+
+    //remove the last element
+    TestVector2.pop_back();
+
+    //erase; remove the element pointed by i
+    vector<int>::iterator j = TestVector2.begin();
+    TestVector2.erase(j);
+
+    //erase all element except last
+    TestVector2.erase(TestVector2.begin(), TestVector2.end() -1);
+
+    //swap 
+    TestVector2.swap(TestVector1);
+
+    //Capacity;
+
+    cout << "\n Capacity " << TestVector2.capacity() << endl;
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
